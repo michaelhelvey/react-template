97cdedf407ef8e5a3dc36d23df0156e4c0ecb05d
Michael Helvey <michael.helvey1@gmail.com>
2023-01-20 10:26:04 -0600

    starting to work

--- src/app.tsx
+++ src/app.tsx
@@ -1,48 +1,50 @@
-import { useReducer } from 'react'
+import { DndProvider, useDrag } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import clsx from 'clsx'
 
-interface Todo {
-	id: number
-	title: string
-	status: 'complete' | 'incomplete'
+function GridItem({ index }: { index: number }) {
+	const [{ isDragging }, drag] = useDrag(() => ({
+		type: 'GRID_ITEM',
+		collect(monitor) {
+			return {
+				isDragging: monitor.isDragging(),
+			}
+		},
+	}))
+
+	return (
+		<div
+			className={clsx(
+				'h-64 w-64 m-1 grid place-items-center',
+				isDragging
+					? 'bg-none'
+					: 'bg-blue-300 font-bold text-2xl text-white'
+			)}
+			ref={drag}
+		>
+			{isDragging ? null : index}
+		</div>
+	)
+}
+
+const ITEMS = Array.from({ length: 8 }, (_, i) => i + 1)
+
+function Grid() {
+	return (
+		<DndProvider backend={HTML5Backend}>
+			<div className="border border-gray-300 rounded shadow p-6 grid grid-flow-row grid-cols-4">
+				{ITEMS.map((num) => (
+					<GridItem key={num} index={num} />
+				))}
+			</div>
+		</DndProvider>
+	)
 }
 
 function App() {
-	const [todo, updateTodo] = useReducer(
-		(state: Todo, update: Partial<Todo>) => {
-			return {
-				...state,
-				...update,
-			}
-		},
-		{ id: 1, title: 'something todo', status: 'incomplete' }
-	)
-
 	return (
 		<div className="h-full w-full grid place-items-center">
-			<div className="border border-gray-300 rounded shadow p-6">
-				<div
-					id="todo"
-					className="p-2 border border-gray-300 rounded flex items-center"
-				>
-					<input
-						type="checkbox"
-						className="mr-2"
-						onChange={(e) =>
-							updateTodo({
-								status: e.target.checked
-									? 'complete'
-									: 'incomplete',
-							})
-						}
-					></input>
-					<input
-						type="text"
-						value={todo.title}
-						className="focus:outline-none"
-						onChange={(e) => updateTodo({ title: e.target.value })}
-					/>
-				</div>
-			</div>
+			<Grid />
 		</div>
 	)
 }
